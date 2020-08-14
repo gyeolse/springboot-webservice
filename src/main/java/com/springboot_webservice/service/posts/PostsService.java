4ffd4622d17ee5683796cca4e7da788774e44906
gyeolse <threewave@kakao.com>
2020-08-14 14:59:07 +0900

    api 요청에 따라 view 보여주기 완료.

--- src/main/java/com/springboot_webservice/service/posts/PostsService.java
+++ src/main/java/com/springboot_webservice/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.springboot_webservice.service.posts;
 
 import com.springboot_webservice.domain.posts.Posts;
 import com.springboot_webservice.domain.posts.PostsRepository;
+import com.springboot_webservice.web.dto.PostsListResponseDto;
 import com.springboot_webservice.web.dto.PostsResponseDto;
 import com.springboot_webservice.web.dto.PostsSaveRequestDto;
 import com.springboot_webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +34,16 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 }
